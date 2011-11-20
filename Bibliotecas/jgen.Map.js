9f4401d345a68e5c5558fcd4ab88a42a807e70a5
Altair123 <la_chivix963@hotmail.com>
2011-11-19 20:52:08 -0500

    diana chavez commit

--- Bibliotecas/jgen.Map.js
+++ Bibliotecas/jgen.Map.js
@@ -1,130 +1,321 @@
-jsface.namespace("jgen");
-
-jsface.def({
-	
-	$meta: {
-		name: "Map",
-		namespace: jgen
+var TSprite = Class.create({
+	map: null,
+	sprite: null,
+	posX: 0,
+	posY: 0,
+	rotationDeg: 0,
+	rotationRad: 0,
+	constructor: function(oMapRef, sClassName) {
+		this.map = oMapRef;
+		this.sprite = document.createElement('div');
+		this.sprite.setStyle({'display': 'none'});
+		this.sprite.className = sClassName;
 	},
-	
-	Map: function(eventQueue) {
-		
-		this.mapData = {};
-		this.scrollX = 0;
-		this.scrollY = 0;
-		this.eventQueue = eventQueue;
-		
-		var viewPort = this.eventQueue.getViewPort();
-		this.viewPort = viewPort.appendChild(
-			jgen.HTML.setStyle(viewPort.ownerDocument.createElement('div'), {
-				'position': 'absolute',
-				'width': '100%',
-				'height': '100%'
-			})
-		);
-		this.tile = jgen.HTML.setStyle(this.viewPort.ownerDocument.createElement('div'), {
-			'position': 'absolute',
-			'background-image': 'url("tile.gif")',
-			'background-repeat': 'no-repeat'
+	onCollide: function() {},
+	onOffScreen: function() {},
+	rotate: function(iAngleDeg) {
+		if (iAngleDeg == this.rotationDeg) return;
+		this.rotationDeg = iAngleDeg;
+		if (this.rotationDeg < 0) this.rotationDeg = 360;
+		if (this.rotationDeg > 360) this.rotationDeg = 0;
+		this.rotationRad = Math.deg2rad(this.rotationDeg);
+		this.sprite.setStyle({
+			'webkit-transform': 'rotate(' + this.rotationDeg + 'deg)'
 		});
 	},
-	
-	setViewPortSize: function(viewPortWidth, viewPortHeight) {
-		this.viewPortWidth = viewPortWidth;
-		this.viewPortHeight = viewPortHeight;
+	rotateTo: function(iAngleDeg) {
+		this.rotate(this.rotationDeg + iAngleDeg);
 	},
-	
-	setMapGeometry: function(mapWidth, mapHeight, tileWidth, tileHeight) {
-		this.mapWidth = mapWidth;
-		this.mapHeight = mapHeight;
-		this.tileWidth = tileWidth;
-		this.tileHeight = tileHeight;
-		jgen.HTML.setStyle(this.tile, {
-			'width': this.tileWidth + 'px',
-			'height': this.tileHeight + 'px',
-			'line-height': this.tileHeight + 'px'
-		});
-	},
-	
-	repaint: function() {
-		if (this.repainting) return;
-		this.repainting = true;
-		this.eventQueue.addCallBack(this, function() {
-			this.render();
-			this.repainting = false;
-		});
-	},
-	
-	scrollTo: function(point) {
-		var scrollX = point[0];
-		var scrollY = point[1];
+	move: function(x, y) {
 		
-		/*
-		scrollX = Math.min(
-			Math.max(scrollX, 0),
-			this.mapWidth * this.tileWidth - this.viewPortWidth
-		);
+		if ((this.posX == x) && (this.posY == y)) return;
 		
-		scrollY = Math.min(
-			Math.max(scrollY, 0),
-			this.mapHeight * this.tileHeight - this.viewPortHeight
-		);
-		*/
-		
-		if ((scrollX != this.scrollX) || (scrollY != this.scrollY)) {
-			if (scrollY < 0) {
-				window.top.status = scrollY;
-			}
-			this.scrollX = scrollX;
-			this.scrollY = scrollY;
-			this.repaint();
-		}
-	},
-	
-	scroll: function(rotation, distance) {
-		this.scrollTo(jgen.Math.pointOfCircle(
-			[this.scrollX, this.scrollY],
-			rotation,
-			distance
-		));
-	},
-	
-	render: function() {
-		var scrollX = this.scrollX;
-		var scrollY = this.scrollY;
-		
-		var xxx = (scrollX % this.tileWidth);
-		var yyy = (scrollY % this.tileHeight);
-		
-		var iFromCell = Math.floor(scrollX / this.tileWidth);
-		var iFromRow = Math.floor(scrollY / this.tileHeight);
-		
-		var iToCell = Math.ceil((this.viewPortWidth + xxx) / this.tileWidth);
-		var iToRow = Math.ceil((this.viewPortHeight + yyy) / this.tileHeight);
-		
-		for (var row = 0; row < iToRow; row++) {
-			for (var cell = 0; cell < iToCell; cell++) {
-				
-				var sIndex = [cell, row].join('.');
-				var oTile = this.mapData[sIndex];
-				if (!oTile) {
-					var tilePosX = (this.tileWidth * cell);
-					var tilePosY = (this.tileHeight * row);
-					oTile = this.mapData[sIndex] = this.viewPort.appendChild(
-						jgen.HTML.setStyle(this.tile.cloneNode(false), {
-							'left': tilePosX + 'px',
-							'top': tilePosY + 'px'
-						}
-					));
-				}
-				// update tile itself
-				//oTile.innerHTML = [iFromCell + cell, iFromRow + row];
-			}
+		if (this.map.isColliding(this, x, y)) {
+			if (this.map.isColliding(this, this.posX, y)) {
+				if (!this.map.isColliding(this, x, this.posY)) this.posX = x;
+			} else this.posY = y;
+		} else {
+			this.posX = x;
+			this.posY = y;
 		}
 		
-		this.viewPort.style.marginLeft = -xxx + 'px';
-		this.viewPort.style.marginTop = -yyy + 'px';
+		if (this.posX < 0) this.onOffScreen();
+		if (this.posY < 0) this.onOffScreen();
+		if (this.posX > this.map.viewPort.offsetWidth) this.onOffScreen();
+		if (this.posY > this.map.viewPort.offsetHeight) this.onOffScreen();
 		
+		this.sprite.setStyle({
+			'left': this.posX + 'px',
+			'top': this.posY + 'px'
+		});
+		
+	},
+	moveTo: function(x, y) {
+		this.move(
+			this.posX + x,
+			this.posY + y
+		);
+	},
+	moveForward: function(px) {
+		this.moveTo(
+			-Math.sin(this.rotationRad) * px,
+			Math.cos(this.rotationRad) * px
+		);
+	},
+	show: function() {
+		this.sprite.setStyle({'display': 'block'});
 	}
+});
+
+var TBullet = Class.create(TSprite, {
+	show: function() {
+		this.base();
+		var oThis = this;
+		this.moving = setInterval(function() {
+			oThis.moveForward(4);
+		}, 0);
+	},
+	onOffScreen: function() {
+		clearInterval(this.moving);
+		this.map.removeSprite(this);
+	},
+	onCollide: function(oSprite) {
+		clearInterval(this.moving);
+		this.map.removeSprite(oSprite);
+		this.map.removeSprite(this);
+	}
+});
+
+var TMap = Class.create({
 	
+	viewPort: null,
+	viewPortBuffer: null,
+	viewPortWidthPx: 0,
+	viewPortHeightPx: 0,
+	viewPortWidthTiles: 0,
+	viewPortHeightTiles: 0,
+	
+	tile: null,
+	tileWidth: 0,
+	tileHeight: 0,
+	tileHalfWidth: 0,
+	tileHalfHeight: 0,
+	
+	mapWidthTiles: 0,
+	mapHeightTiles: 0,
+	
+	sprites: [],
+	spritesViewPort: null,
+	
+	constructor: function(oViewPort, iViewPortWidth, iViewPortHeight) {
+		var oViewPortElement = oViewPort.ownerDocument.createElement('div').setStyle({
+			'position': 'absolute',
+			'width': '100%',
+			'height': '100%'
+		});
+		this.viewPort = oViewPort.appendChild(oViewPortElement);
+		this.spritesViewPort = oViewPort.appendChild(oViewPortElement.cloneNode(true));
+		this.viewPortWidthPx = iViewPortWidth;
+		this.viewPortHeightPx = iViewPortHeight;
+		this.viewPortBuffer = document.createDocumentFragment();
+	},
+	
+	loadMap: function(sMapUrl, fCallBack) {
+		var oThis = this;
+		var oXMLHttpRequest = new XMLHttpRequest;
+		oXMLHttpRequest.open("GET", sMapUrl, true);
+		oXMLHttpRequest.onreadystatechange = function() {
+			if (this.readyState != XMLHttpRequest.DONE) return;
+			oThis.processMap(this.responseXML.documentElement);
+			fCallBack.call(oThis);
+		}
+		oXMLHttpRequest.send(null);
+	},
+	
+	processMap: function(oMapData) {
+		this.tileWidth = parseInt(oMapData.getAttribute('tile-width'), 10);
+		this.tileHeight = parseInt(oMapData.getAttribute('tile-height'), 10);
+		
+		this.tileHalfWidth = (this.tileWidth / 2);
+		this.tileHalfHeight = (this.tileHeight / 2);
+		this.viewPortWidthTiles = Math.ceil(this.viewPortWidthPx / this.tileWidth);
+		this.viewPortHeightTiles = Math.ceil(this.viewPortHeightPx / this.tileHeight * 2);
+		this.mapWidthTiles = parseInt(oMapData.getAttribute('map-width'), 10);
+		this.mapHeightTiles = parseInt(oMapData.getAttribute('map-height'), 10);
+		
+		var aCSSRules = [];
+		
+		var aMapDefinition = oMapData.getElementsByTagName('map');
+		for (var i = 0; i < aMapDefinition.length; i++) {
+			var aMapDefinitions = aMapDefinition[i].getElementsByTagName('*');
+			for (var j = 0; j < aMapDefinitions.length; j++) {
+				var oMapDefinition = aMapDefinitions[j];
+				if (oMapDefinition.tagName.toLowerCase() == 'sprite') {
+					var sSpriteRef = oMapDefinition.getAttribute('ref');
+					var iSpritePosX = parseInt(oMapDefinition.getAttribute('x'), 10);
+					var iSpritePosY = parseInt(oMapDefinition.getAttribute('y'), 10);
+					var iSpriteRotate = parseInt(oMapDefinition.getAttribute('rotate'), 10);
+					var sSpriteClass = oMapDefinition.getAttribute('class');
+					if (!sSpriteClass) sSpriteClass = 'TSprite';
+					var oSpriteClass = (eval(sSpriteClass));
+					var oSprite = this.addSprite(new oSpriteClass(this, 'sprite-' + sSpriteRef));
+					oSprite.move(iSpritePosX, iSpritePosY);
+					if (iSpriteRotate) oSprite.rotate(iSpriteRotate);
+					oSprite.show();
+				}
+			}
+		}
+		
+		var aSpriteDefinition = oMapData.getElementsByTagName('sprites');
+		for (var i = 0; i < aSpriteDefinition.length; i++) {
+			var aSpriteDefinitions = aSpriteDefinition[i].getElementsByTagName('sprite');
+			for (var j = 0; j < aSpriteDefinitions.length; j++) {
+				var oSpriteDefinition = aSpriteDefinitions[j];
+				var sSpriteName = oSpriteDefinition.getAttribute('name');
+				var sSpriteUrl = oSpriteDefinition.getAttribute('url');
+				var sSpriteStyle = oSpriteDefinition.getAttribute('style');
+				var iSpriteWidth = parseInt(oSpriteDefinition.getAttribute('width'), 10);
+				var iSpriteHeight = parseInt(oSpriteDefinition.getAttribute('height'), 10);
+				aCSSRules.push('.sprite-' + sSpriteName + '{' +
+					'position:absolute;' +
+					'width:' + iSpriteWidth + 'px;' +
+					'height:' + iSpriteHeight + 'px;' +
+					(sSpriteUrl ? 'background-image:url("' + sSpriteUrl + '");' : '') +
+					(sSpriteStyle ? sSpriteStyle : '') +
+				'}');
+			}
+		}
+		
+		var aTileDefinition = oMapData.getElementsByTagName('tiles');
+		for (var i = 0; i < aTileDefinition.length; i++) {
+			var aTileDefinitions = aTileDefinition[i].getElementsByTagName('tile');
+			for (var j = 0; j < aTileDefinitions.length; j++) {
+				var oTileDefinition = aTileDefinitions[j];
+				var sTileName = oTileDefinition.getAttribute('name');
+				var sTileUrl = oTileDefinition.getAttribute('url');
+				var iTileWidth = parseInt(oTileDefinition.getAttribute('width'), 10);
+				var iTileHeight = parseInt(oTileDefinition.getAttribute('height'), 10);
+				if (!iTileWidth) iTileWidth = this.tileWidth;
+				if (!iTileHeight) iTileHeight = this.tileHeight;
+				aCSSRules.push('.tile-' + sTileName + '{' +
+					'position:absolute;' +
+					'background-repeat:no-repeat;' +
+					'background-position:center top;' +
+					'width:' + iTileWidth + 'px;' +
+					'height:' + iTileHeight + 'px;' +
+					'background-image:url("' + sTileUrl + '");' +
+				'}');
+			}
+		}
+		
+		var oOwnerDocument = this.viewPort.ownerDocument;
+		var oHead = oOwnerDocument.getElementsByTagName('head')[0];
+		var oStyle = oOwnerDocument.createElement('style');
+		oStyle.setAttribute('type', 'text/css');
+		oStyle.innerText += aCSSRules.join('');
+		oHead.appendChild(oStyle);
+		
+		this.tile = document.createElement('div');
+		this.tile.className = 'tile-grass1';
+	},
+	
+	calcViewPort: function(iScrollX, iScrollY) {
+		return {
+			fromRow: Math.ceil(iScrollY / this.tileHalfHeight) - 1,
+			toRow: Math.ceil((iScrollY + this.viewPortHeightPx) / this.tileHalfHeight) + 1,
+			fromCellEven: Math.round(iScrollX / this.tileWidth),
+			toCellEven: Math.round((iScrollX + this.viewPortWidthPx) / this.tileWidth) + 1,
+			fromCellOdd: Math.round((iScrollX - this.tileHalfWidth) / this.tileWidth),
+			toCellOdd: Math.round((iScrollX + this.viewPortWidthPx + this.tileHalfWidth) / this.tileWidth)
+		};
+	},
+	
+	clearViewPort: function() {
+		var oViewPort = this.viewPort.cloneNode(false);
+		this.viewPort.parentNode.replaceChild(oViewPort, this.viewPort);
+		this.viewPort = oViewPort;
+	},
+	
+	screen2Map: function(clientX, clientY, iScrollX, iScrollY) {
+		var sx = clientX + iScrollX;  
+		var sy = clientY + iScrollY;
+		var xmouseA = Math.round(sx / this.tileWidth + sy / this.tileHeight);
+		var ymouseA = Math.round(-sx / this.tileWidth + sy / this.tileHeight);
+		var iRow = (xmouseA + ymouseA);
+		var iCell = Math.round(iRow % 2 ?
+			(sx - this.tileHeight) / this.tileWidth :
+			(sx / this.tileWidth)
+		);
+		return [iCell, iRow];
+	},
+	
+	addSprite: function(oSprite) {
+		this.sprites.push(oSprite);
+		oSprite.sprite = this.spritesViewPort.appendChild(
+			this.spritesViewPort.ownerDocument.importNode(
+				oSprite.sprite
+			)
+		);
+		return oSprite;
+	},
+	
+	removeSprite: function(oSprite) {
+		for (var c = 0; c < this.sprites.length; c++) {
+			if (this.sprites[c] == oSprite) {
+				this.sprites.splice(c, 1);
+				oSprite.sprite.parentNode.removeChild(oSprite.sprite);
+				break;
+			}
+		}
+	},
+	
+	isColliding: function(oSprite, x, y) {
+		for (var c = 0; c < this.sprites.length; c++) {
+			if (this.sprites[c] != oSprite) {
+				var oTargetSprite = this.sprites[c];
+				var iTargetDistance = Math.ceil(Math.point_distance(
+					(x + oSprite.sprite.offsetWidth / 2),
+					(y + oSprite.sprite.offsetHeight / 2),
+					(oTargetSprite.posX + oTargetSprite.sprite.offsetWidth / 2),
+					(oTargetSprite.posY + oTargetSprite.sprite.offsetHeight / 2)
+				));
+				if (iTargetDistance < 30) {
+					oSprite.onCollide(oTargetSprite);
+					return true;
+				}
+			}
+		}
+	},
+	
+	render: function(iScrollX, iScrollY) {
+		var oNewMapRect = this.calcViewPort(iScrollX, iScrollY);
+		var oFragment = this.viewPortBuffer.cloneNode(false);
+		var iFromRow = oNewMapRect.fromRow;
+		var iToRow = oNewMapRect.toRow;
+		var iTileOffsetX = (this.tileHalfWidth + iScrollX);
+		var iTileOffsetY = (this.tileHalfHeight + iScrollY);
+		for (var iRow = iFromRow; iRow < iToRow; iRow++) {
+			if (iRow % 2) {
+				var iOffset = this.tileHalfWidth;
+				var iFromCell = oNewMapRect.fromCellOdd;
+				var iToCell = oNewMapRect.toCellOdd;
+			} else {
+				var iOffset = 0;
+				var iFromCell = oNewMapRect.fromCellEven;
+				var iToCell = oNewMapRect.toCellEven;
+			}
+			var iTileTop = (this.tileHeight * iRow - this.tileHalfHeight * iRow) - iTileOffsetY;
+			for (var iCell = iFromCell; iCell < iToCell; iCell++) {
+				var iTileLeft = (this.tileWidth * iCell + iOffset) - iTileOffsetX;
+				var oTile = this.tile.cloneNode(false);
+				oTile.style.left = iTileLeft + 'px';
+				oTile.style.top = iTileTop + 'px';
+				oFragment.appendChild(oTile);
+			}
+		}
+		this.spritesViewPort.style.marginLeft = -iScrollX + this.viewPortWidthPx / 2 + 'px';
+		this.spritesViewPort.style.marginTop = -iScrollY + this.viewPortHeightPx / 2 + 'px';
+		this.clearViewPort();
+		this.viewPort.appendChild(oFragment);
+	}
 });
